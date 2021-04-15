aec0f5ce21e24f9ddead1ef873317d8c3c9e2851
QuangBuiYT <quangbui@pc.localdomain>
2021-04-15 23:07:44 +0700

    Committed from cmd

--- bld.cmd
+++ bld.cmd
@@ -1,5 +1,5 @@
 @echo off
-set flags=g++ -std=c++17 -o "%1" "%1.cpp" -DLOCAL -g -ggdb3 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -D_GLIBCXX_ASSERTIONS -fno-sanitize-recover -fstack-protector
+set flags=g++ -std=c++17 -o "%1" "%1.cpp" -DLOCAL -ggdb3 -Wall -Wextra -pedantic -Wshadow -Wformat=2 -Wfloat-equal -Wconversion -Wlogical-op -Wshift-overflow=2 -Wduplicated-cond -Wcast-qual -Wcast-align -D_GLIBCXX_DEBUG -D_GLIBCXX_DEBUG_PEDANTIC -D_FORTIFY_SOURCE=2 -D_GLIBCXX_ASSERTIONS -fno-sanitize-recover -fstack-protector
 echo %flags%
 
 echo.
